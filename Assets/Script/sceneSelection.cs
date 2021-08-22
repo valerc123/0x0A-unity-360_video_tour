2649dcb786007869cc55177b4cac8b02f9ca5685
valerc123 <valentina.rua123@gmail.com>
2021-08-22 02:15:33 -0500

    Initial commit

---
+++ Assets/Script/sceneSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSelection : MonoBehaviour
+{
+    public GameObject canvas;
+    public bool switchMessage = false;
+    void Start(){
+        canvas.SetActive(false);
+    }
+    public void sceneSelected(int scene){
+        if(scene == 0){
+            SceneManager.LoadScene("Cantina");
+        }else if(scene == 1){
+            SceneManager.LoadScene("Cube");
+        }else if(scene == 2){
+            SceneManager.LoadScene("360VideoTour");
+        }else if(scene == 3){
+            SceneManager.LoadScene("Mezzanine");
+        }
+    }
+    public void openMessage () {
+        switchMessage = !switchMessage;
+        canvas.SetActive(switchMessage);
+    }
+}
